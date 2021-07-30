16fe62c35a5fae163e7dd94422e74b3b64809c14
mpontoc <mpontoc@hotmail.com>
2021-07-30 16:41:39 -0300

    cucumber5 test rerun surefire

--- src/test/java/com/example/demo/steps/IOS_MobileTests_Steps.java
+++ src/test/java/com/example/demo/steps/IOS_MobileTests_Steps.java
@@ -1,14 +1,35 @@
 package com.example.demo.steps;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+import com.example.demo.User;
+
 import br.com.mpontoc.picaroon.core.commands.ActionsCommands;
+import br.com.mpontoc.picaroon.core.utils.Log;
+import io.cucumber.core.gherkin.Scenario;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Entao;
+import io.cucumber.plugin.event.Result;
 
 public class IOS_MobileTests_Steps {
+	
+	@Value("${prop.valor:valorDefault}")
+	private String prop;
+	
+	@Autowired
+	User user;
 
 	@Dado("que eu estou no device ios")
 	public void que_eu_estou_no_device_ios() {
 		System.out.println("ïos");
+		
+		Log.log(prop);
+		
+		user.setName("Teste");	
+		System.out.println(user.getName());
+		
+		
 	}
 
 	@Entao("valido o texto")
